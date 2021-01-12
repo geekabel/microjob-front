692151ad7362743e65913e6fa31da814e50e3a86
Godwin <koffigodwin96@gmail.com>
2021-01-12 14:24:03 +0100

    microjob first commit + menu link + Auth +search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import axios from 'axios';
+import "./App.css";
+class App extends  Component {
+  // State of your application
+  state = {
+    microjobs: [],
+    error: null,
+    query: '',
+  };
+
+  // Fetch your microjobs immediately after the component is mounted
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get('http://localhost:1337/microjobs');
+      this.setState({ microjobs: response.data });
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  render() {
+    const { error, microjob } = this.state;
+
+    // Print errors if any
+    if (error) {
+      return <div>Tu as fais: {error.message}</div>;
+    }
+
+    return (
+      
+      <div className="App container d-flex mx-2">
+         {this.state.microjobs.map((microjob,id) => (
+           <div key={id} className="card taille "> 
+           <img className="card-img-top" src={`http://localhost:1337${microjob.image.formats.thumbnail.url}`} alt="les thumnails"/>
+
+            <h5>{microjob.title} -{microjob.location}</h5>
+
+            <div className="card-body"> 
+              <p className="card-text">{microjob.description}</p>
+              <p><span>a partir de </span>{microjob.price} <span>FRCFA</span></p>
+              <h2 className="card-text"></h2>
+            </div>
+            
+            </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/auth/Signin.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Signin extends Component{
+  
+
+    render() {
+        return (
+            <div>SignIn</div> 
+        )
+    }
+
+}
+
+
+export default Signin;
